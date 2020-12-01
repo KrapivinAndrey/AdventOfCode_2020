a64355b848afe43bca9a810c3f3a0f33f997c01b
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-01 11:39:35 +0500

    Второе задание

--- day01/main.py
+++ day01/main.py
@@ -5,4 +5,10 @@ with open('input.txt', 'r') as reader:
 
 answer = [x * y for x, y in product(in_data, in_data) if x+y == 2020]
 
+# first
+print(*answer)
+
+answer = [x * y * z for x, y, z in product(in_data, in_data, in_data) if x+y+z == 2020]
+
+# second
 print(*answer)
