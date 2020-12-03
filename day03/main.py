5da9635fe5a5ccd3b5de15ea549b8209e417274f
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-03 13:42:53 +0500

    Second task

--- day03/main.py
+++ day03/main.py
@@ -1,15 +1,19 @@
 with open('input.txt', 'r') as reader:
     in_data = [x for x in reader.read().split('\n')]
 
-
-dx = 3
 length = len(in_data[0])
-x = 0
-trees = 0
 
-for y in range(len(in_data)):
-    if in_data[y][x] == "#":
-        trees += 1
-    x = (x + dx) % length
 
-print(trees)
+def count_trees(dx, dy):
+    x = y = trees = 0
+    while y < len(in_data):
+        if in_data[y][x] == "#":
+            trees += 1
+        x = (x + dx) % length
+        y += dy
+
+    return trees
+
+
+res = count_trees(1, 1) * count_trees(3, 1) * count_trees(5, 1) * count_trees(7, 1) * count_trees(1, 2)
+print(res)
