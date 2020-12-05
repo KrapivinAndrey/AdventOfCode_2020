a40e7bef4407c5b03c93317170ac03f61d1b46ed
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-05 22:43:45 +0500

    First task

---
+++ day05/main.py
@@ -0,0 +1,23 @@
+with open('input.txt', 'r') as reader:
+    in_data = [x for x in reader.read().split('\n')]
+
+
+def get_seat(path: str) -> int:
+    print(path)
+    row = list(range(0, 128))
+    column = list(range(0, 8))
+
+    for dir in path:
+        if dir == 'F':
+            row = row[0:len(row) // 2]
+        elif dir == 'B':
+            row = row[len(row) // 2:]
+        elif dir == 'L':
+            column = column[0:len(column) // 2]
+        elif dir == 'R':
+            column = column[len(column) // 2:]
+
+    return row[0] * 8 + column[0]
+
+
+print(max([get_seat(x) for x in in_data]))
