43d76ce08af1f10dbc5689197360f35a198cb96d
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-06 21:08:23 +0500

    second task

--- day06/main.py
+++ day06/main.py
@@ -5,15 +5,19 @@ with open('input.txt', 'r') as reader:
 def count_answer_yes(declaration:str)->int:
 
     check = {}
+    size_of_group = 0
     for group in declaration.split('\n'):
+        size_of_group += 1
         for answer in group:
-            check[answer] = 1
+            if answer not in check:
+                check[answer] = 1
+            else:
+                check[answer] += 1
 
-    return len(check)
+    return len([x for x in check.values() if x == size_of_group])
 
 
 res = 0
 for x in in_data:
-    res+=count_answer_yes(x)
-
+    res += count_answer_yes(x)
 print(res)
