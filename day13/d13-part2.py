2f10278d01a759a84fb9faa89a35fa54066716ca
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-13 21:09:08 +0500

    Second task

---
+++ day13/d13-part2.py
@@ -0,0 +1,41 @@
+from functools import reduce
+
+with open('input.txt', 'r') as reader:
+    mark = int(reader.readline())
+    buses = [x for x in reader.readline().split(',')]
+
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod//n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
+n = [] # делитель
+a = [] # остаток
+
+for i in range(len(buses)):
+    if buses[i] == 'x':
+        continue
+    else:
+        n.append(int(buses[i]))
+        a.append(-i)
+
+print(chinese_remainder(n, a))
