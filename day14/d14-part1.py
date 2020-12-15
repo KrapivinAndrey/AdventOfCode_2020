d39366192748d81fb7cf6bd58f8d58726ee525fb
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-15 22:58:34 +0500

    First task

---
+++ day14/d14-part1.py
@@ -0,0 +1,13 @@
+start = [1, 12, 0, 20, 8, 16]
+been  = {}
+for i in range(1, len(start) + 1):
+    been[start[i-1]] = i
+num = 0
+for i in range(len(start) + 1, 2021):
+    print("Turn {}: num {}".format(i, num))
+    prev = num
+    if num not in been:
+        num = 0
+    else:
+        num = i - been[num]
+    been[prev] = i
