171c446d6ce70aee3c3ec21fcc22540603cb36d1
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-19 00:12:52 +0500

    А вот так быстрее

--- day17/d17-part1.py
+++ day17/d17-part1.py
@@ -1,42 +1,45 @@
 import numpy as np
 
-matrix = np.zeros((100, 100, 100))
+matrix = np.zeros((50, 50, 50, 50))
 
 with open('input.txt', 'r') as reader:
     in_data = [x for x in reader.readlines()]
 
 
-def count_neighbors(x,y,z,matrix):
+def count_neighbors(x, y, z, w, matrix):
     result = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
             for k in range(-1, 2):
-                if i == j == k == 0:
-                    continue
-                if matrix[x+i,y+j,z+k] == 1:
-                    result += 1
-
+                for l in range(-1, 2):
+                    if i == j == k == l == 0:
+                        continue
+                    if matrix[x + i, y + j, z + k, w + l] == 1:
+                        result += 1
     return result
 
 
-def move(matrix):
-    result = np.zeros((100, 100, 100))
-    for x in range(1, 99):
-        for y in range(1, 99):
-            for z in range(1, 99):
-                if matrix[x, y, z] == 1 and count_neighbors(x, y, z, matrix) in [2, 3]:
-                    result[x, y, z] = 1
-                elif matrix[x, y, z] == 0 and count_neighbors(x, y, z, matrix) == 3:
-                    result[x, y, z] = 1
+def move(temp, radius):
+    result = np.zeros((50, 50, 50, 50))
+    for x in range(25-radius-1, 25 + radius + 2):
+        for y in range(25-radius-1, 25 + radius + 2):
+            for z in range(25-radius-1, 25 + radius + 2):
+                for w in range(25-radius-1, 25 + radius + 2):
+                    if temp[x, y, z, w] == 1 and count_neighbors(x, y, z, w, temp) in [2, 3]:
+                        result[x, y, z, w] = 1
+                    elif temp[x, y, z, w] == 0 and count_neighbors(x, y, z, w, temp) == 3:
+                        result[x, y, z, w] = 1
+    print(np.count_nonzero(result))
     return result
 
 
+radius = len(in_data) // 2
+print(radius)
 for i in range(len(in_data)):
     for j in range(len(in_data)):
         if in_data[i][j] == '#':
-            matrix[50+i, 50+j, 50] = 1
+            matrix[25-radius+i, 25-radius+j, 25, 25] = 1
 
 for i in range(6):
-    matrix = move(matrix)
-
-print(np.count_nonzero(matrix))
+    matrix = move(matrix, radius)
+    radius += 1
