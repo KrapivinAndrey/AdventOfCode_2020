3ce9af1f200818399d72186fbfd5e4ce892b2d77
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2020-12-19 02:19:31 +0500

    Более странная ОПЗ

---
+++ day18/d18-part2.py
@@ -0,0 +1,51 @@
+def calculate(expression)->int:
+
+    def convert(str):
+        lst = [x for x in list(str) if x != ' ']
+        out = ""
+        stack = []
+        for x in lst:
+            if x.isdigit():
+                out += x
+            elif x == '(':
+                stack.append(x)
+            elif x == ')':
+                while True:
+                    t = stack.pop()
+                    if t != '(':
+                        out += t
+                    else:
+                        break
+            else:
+                while  stack != [] and stack[-1] != '(' and x == '*' and stack[-1] == '+':
+                    out += stack.pop()
+                stack.append(x)
+        while (len(stack) > 0):
+            out += stack.pop()
+        return out
+
+
+    def polska(srt):
+        import operator
+        OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+        stack = []
+        lst = list(srt)
+        for i in srt:
+            if i.isdigit():
+                stack.append(i)
+                lst.remove(i)
+            else:
+                cnt1, cnt2 = stack.pop(), stack.pop()
+                stack.append(OPERATORS[i](int(cnt2), int(cnt1)))
+                lst.remove(i)
+        return stack.pop()
+
+    polska_str = convert(expression)
+    return polska(polska_str)
+
+
+with open('input.txt', 'r') as reader:
+    result = sum([calculate(x) for x in reader.read().split('\n')])
+
+print(result)
